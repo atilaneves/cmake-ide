dd2d28577a0ff6dafb0f1a7385814ce7d75695c4
David Vanderhaeghe <github@dlyr.fr>
2017-12-18 14:52:27 +0100

    type in function call

--- cmake-ide.el
+++ cmake-ide.el
@@ -305,7 +305,7 @@ This works by calling cmake in a temporary directory (or cmake-ide-build-dir)
 	      (let ((default-directory cmake-dir))
 		(cmake-ide--run-cmake-impl project-dir cmake-dir)
 		(cmake-ide--register-callback))))
-	(cmake-ide-message "try to run cmake on a non cmake project [%s]" default-directory)
+	(cmake-ide--message "try to run cmake on a non cmake project [%s]" default-directory)
 	)))))
 
 
